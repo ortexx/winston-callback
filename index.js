a0a2f2e4cbeac5cc878ee9b089651d29cc7c68a6
Ortex <mywebname@mail.ru>
2016-10-02 13:13:54 +0300

    fix error handling

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const winston = require('winston');
 let Logger = winston.Logger;
 let old = Logger.prototype.log;
     
-Logger.prototype.log = function() {
+Logger.prototype.log = function(level) {
     let keys = Object.keys(this.transports);
-    let countAll = keys.length;
+    let countAll = 0;
     let countLogged = 0;
     let args = [].slice.call(arguments);    
     let callback = typeof args[args.length - 1] == 'function'? args[args.length - 1]: false;
@@ -25,12 +25,16 @@ Logger.prototype.log = function() {
     
     keys.map((key) => {
         let transport = this.transports[key];
+       
+        if(this.levels[transport.level] >= this.levels[level]) {
+            countAll += 1;
+        }
         
         let onLogged = () => {
-            countLogged++;            
+            countLogged++;                       
             transport.removeListener('logged', onLogged);
             
-            if(countAll == countLogged) {
+            if(countAll <= countLogged) {
                 callback();
             }
         }
